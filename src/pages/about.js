cdeacd0cb9d50dbdce6a388c6801bf8be6326270
Toby Fee <tobyfee@gmail.com>
2018-12-27 12:14:43 -0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+
+  <div style={{ color: `teal` }}>
+  <Header headerText="importation is confusing!" />
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+  </div>
+)
